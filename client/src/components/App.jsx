bbc09734e559395a390d6519ecb706f083777797
Jesse Bell <jkurtasbell@gmail.com>
2019-06-25 18:57:57 -0600

    Display Basic Form Asking For Park Info

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,12 +3,48 @@ import 'react';
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      parkName: '',
+      campsite: '',
+      checkInDate: '',
+      checkOutDate: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit () {
+
+
   }
 
   render() {
     return (
       <div>
-        <p>Hello!</p>
+        <h1>CampTogether</h1>
+        <form onSubmit={this.handleSubmit()}>
+          <div>
+          <label for="park-name">Enter Park Name</label>
+          <input type="text" name="park-name" required></input>
+          </div>
+          <div>
+          <label for="campsite-number">Enter Campsite</label>
+          <input type="text" name="campsite"></input>
+          </div>
+          <div>
+            <label for="start-date">Enter Check-In Date</label>
+            <input type="date" name="start-date"></input>
+          </div>
+          <div>
+            <label for="end-date">Enter Check-Out Date</label>
+            <input type="date" name="end-date"></input>
+          </div>
+          <div>
+            <input type="submit" value="Submit"></input>
+          </div>
+        </form>
       </div>
     )
   }
--- server/app.js
+++ server/app.js
@@ -4,9 +4,6 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 
-// app.get('/', (req, res) => {
-//   res.send('Hi from GET request');
-// })
 
 app.listen(4000, () => {
   console.log('Listening on 4000')
